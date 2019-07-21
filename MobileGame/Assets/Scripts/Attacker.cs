6f21960c567dcede14d9537cb76e69015983b735
BMarcelus <bmdizon@uci.edu>
2019-07-20 18:37:52 -0700

    tower and rock art added to game

--- MobileGame/Assets/Scripts/Attacker.cs
+++ MobileGame/Assets/Scripts/Attacker.cs
@@ -11,6 +11,7 @@ public class Attacker : MonoBehaviour
   public float attackSpeed = 1;
   private float attackTimer = 0.2f;
   public float damage = 5;
+  private GameObject Model;
 
   public GameEvent onPlayerDamaged;
     // Start is called before the first frame update
@@ -18,6 +19,7 @@ public class Attacker : MonoBehaviour
     {
         player = GameObject.Find("Player");
         rb = GetComponent<Rigidbody>();
+        Model = transform.Find("Model").gameObject;
     }
 
     // Update is called once per frame
@@ -26,15 +28,22 @@ public class Attacker : MonoBehaviour
         Vector3 diff = player.transform.position - transform.position;
         diff.y = 0;
         rb.velocity = diff.normalized * speed;
+        
+        transform.rotation = Quaternion.LookRotation(rb.velocity);
+        Model.transform.rotation=transform.rotation;
+        Model.transform.Rotate(0,0,Mathf.Cos(Time.fixedTime*6)*10);
+
         if(attacking) {
+          Model.transform.Rotate(-30+attackTimer*60,0,0);
+          Model.transform.localPosition = new Vector3(
+            0,0,-.5f+attackTimer*1
+          );
           attackTimer -= Time.deltaTime;
           if(attackTimer<=0) {
             attackTimer = attackSpeed;
             Attack();
           }
         }
-        transform.rotation = Quaternion.LookRotation(rb.velocity);
-        transform.Rotate(0,0,Mathf.Cos(Time.fixedTime*6)*10);
     }
 
     void Attack() {
