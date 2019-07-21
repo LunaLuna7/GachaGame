ed4519b16b61a4cba7380cc3319620ef827f6489
Luis <lfluna@uci.edu>
2019-07-20 17:09:47 -0700

    Event system

---
+++ MobileGame/Assets/LunaWorkSpace/Scripts/Primitive/FloatReference.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+//-////////////////////////////////////////////////////
+///
+/// Float wrapper that is use to provide more functionality to
+/// FloatValue by adding the option of using a constant value
+/// instead of the Scriptable Obj
+///
+[Serializable]
+public class FloatReference
+{
+    public bool useConstant = true;
+    public float ConstantValue;
+    public FloatVariable Variable;
+
+    public float Value
+    {
+        get { return useConstant ? ConstantValue : Variable.RuntimeValue; }
+        set {Variable.RuntimeValue = value;}
+    }
+
+}
